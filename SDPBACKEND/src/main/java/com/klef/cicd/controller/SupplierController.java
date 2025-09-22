d849ea2690cfe576ec315862ab9f6f4b76ab1043
narayana <mandavalakshminarayana446@gmail.com>
2025-09-22 20:17:00 +0530

    updated backend v1.0

--- SDPBACKEND/src/main/java/com/klef/cicd/controller/SupplierController.java
+++ SDPBACKEND/src/main/java/com/klef/cicd/controller/SupplierController.java
@@ -1,13 +1,23 @@
 package com.klef.cicd.controller;
 
-import com.klef.cicd.model.Supplier;
-import com.klef.cicd.service.SupplierService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.cicd.model.Supplier;
+import com.klef.cicd.service.SupplierService;
 
 @RestController
 @RequestMapping("/api/suppliers")
@@ -69,7 +79,7 @@ public class SupplierController {
         return ResponseEntity.ok(suppliers);
     }
     
-    // Inner class for error responses
+    
     public static class ErrorResponse {
         private String message;
         
